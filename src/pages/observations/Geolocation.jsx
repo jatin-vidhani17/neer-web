60ba846939a57bb88a977b4a3646868b3c9e8179
Jatin Vidhani <131987761+jatin-vidhani17@users.noreply.github.com>
2024-10-15 12:35:44 +0530

    Update Geolocation.jsx

--- src/pages/observations/Geolocation.jsx
+++ src/pages/observations/Geolocation.jsx
@@ -1,104 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Import marker icon images
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const createColoredIcon = (color) => {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 25;
+  canvas.height = 41;
+
+  const img = new Image();
+  img.src = markerIcon;
+
+  return new Promise((resolve) => {
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0);
+      ctx.globalCompositeOperation = 'source-in';
+      ctx.fillStyle = color;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      const iconUrl = canvas.toDataURL();
+      resolve(L.icon({
+        iconUrl: iconUrl,
+        iconRetinaUrl: iconUrl,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowUrl: markerShadow,
+      }));
+    };
+  });
+};
 
 const GeoLocation = () => {
-  const initMap = () => {
-    const map = new window.google.maps.Map(document.getElementById("map"), {
-      center: new window.google.maps.LatLng(-33.91722, 151.23064),
-      zoom: 16,
-    });
+  const [markerIcons, setMarkerIcons] = useState([]);
 
-    const iconBase =
-      "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
-    const icons = {
-      parking: {
-        icon: iconBase + "parking_lot_maps.png",
-      },
-      library: {
-        icon: iconBase + "library_maps.png",
-      },
-      info: {
-        icon: iconBase + "info-i_maps.png",
-      },
+  const features = [
+    { position: [23.0200, 72.5645], type: "Near Sabarmati Riverfront", color: "#FF5733" }, // Decent Red
+    { position: [23.0225, 72.5700], type: "Near Gandhi Ashram", color: "#33FF57" }, // Decent Green
+    { position: [23.0250, 72.5775], type: "Near Sardar Bridge", color: "#3357FF" }, // Decent Blue
+    { position: [23.0280, 72.5800], type: "Near Ellis Bridge", color: "#DFAB1D" }, // Decent Pink
+    { position: [23.0310, 72.5725], type: "Near Nehru Bridge", color: "#FF9F33" }, // Decent Orange
+    { position: [23.0340, 72.5665], type: "Near Dandi Bridge", color: "#33FFF0" }, // Decent Cyan
+    { position: [23.0360, 72.5750], type: "Near Sardar Patel Bridge", color: "#FFA500" }, // Decent Dark Orange
+    { position: [23.0390, 72.5780], type: "Near Kankaria Lake", color: "#800080" }, // Decent Purple
+    { position: [23.0415, 72.5730], type: "Near Usmanpura", color: "#A52A2A" }, // Decent Brown
+    { position: [23.0450, 72.5700], type: "Near Law Garden", color: "#008000" }, // Decent Dark Green
+  ];
+
+  useEffect(() => {
+    const loadIcons = async () => {
+      const icons = await Promise.all(features.map(feature => createColoredIcon(feature.color)));
+      setMarkerIcons(icons);
     };
-    const features = [
-      { position: new window.google.maps.LatLng(-33.91721, 151.2263), type: "info" },
-      { position: new window.google.maps.LatLng(-33.91539, 151.2282), type: "info" },
-      { position: new window.google.maps.LatLng(-33.91747, 151.22912), type: "info" },
-      { position: new window.google.maps.LatLng(-33.9191, 151.22907), type: "info" },
-      { position: new window.google.maps.LatLng(-33.91725, 151.23011), type: "info" },
-      { position: new window.google.maps.LatLng(-33.91872, 151.23089), type: "info" },
-      { position: new window.google.maps.LatLng(-33.91784, 151.23094), type: "info" },
-      { position: new window.google.maps.LatLng(-33.91682, 151.23149), type: "info" },
-      { position: new window.google.maps.LatLng(-33.9179, 151.23463), type: "info" },
-      { position: new window.google.maps.LatLng(-33.91666, 151.23468), type: "info" },
-      { position: new window.google.maps.LatLng(-33.916988, 151.23364), type: "info" },
-      { position: new window.google.maps.LatLng(-33.91662347903106, 151.22879464019775), type: "parking" },
-      { position: new window.google.maps.LatLng(-33.916365282092855, 151.22937399734496), type: "parking" },
-      { position: new window.google.maps.LatLng(-33.91665018901448, 151.2282474695587), type: "parking" },
-      { position: new window.google.maps.LatLng(-33.919543720969806, 151.23112279762267), type: "parking" },
-      { position: new window.google.maps.LatLng(-33.91608037421864, 151.23288232673644), type: "parking" },
-      { position: new window.google.maps.LatLng(-33.91851096391805, 151.2344058214569), type: "parking" },
-      { position: new window.google.maps.LatLng(-33.91818154739766, 151.2346203981781), type: "parking" },
-      { position: new window.google.maps.LatLng(-33.91727341958453, 151.23348314155578), type: "library" },
-    ];
 
-    // Create markers.
-    for (let i = 0; i < features.length; i++) {
-      new window.google.maps.Marker({
-        position: features[i].position,
-        icon: icons[features[i].type].icon,
-        map: map,
-      });
-    }
-  };
-
-  const loadScript = (url) => {
-    const script = document.createElement("script");
-    script.src = url;
-    script.async = true;
-    script.defer = true;
-    script.onload = initMap; // Initialize the map once the script loads
-    document.body.appendChild(script);
-  };
-
-  React.useEffect(() => {
-    loadScript(
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBa37EdrOvxvXcegEsfxxm9YGOD8fQNZCQ&callback=initMap&v=weekly"
-    );
+    loadIcons();
   }, []);
 
   return (
-    <div>
+    <div className="mt-20">
       <header>
         {/* Include your header component here */}
       </header>
       <main className="flex flex-col items-center py-12">
         <div className="container w-4/5">
           <div className="head flex flex-row items-end justify-end bg-gray-100 p-2 mb-4">
-            <select className="h-8 mx-1">
-              <option value="" selected disabled>
+            <select className="h-8 mx-1" defaultValue="">
+              <option value="" disabled>
                 Select Field
               </option>
               <option value="">Date</option>
-              <option>Time</option>
-              <option>City</option>
-              <option>Temp.</option>
-              <option>PH</option>
-              <option>Depth</option>
-              <option>O<sub>2</sub></option>
-              <option>Conductivity</option>
-              <option>Turbidity</option>
-              <option>Chlorophyll</option>
-              <option>SPM</option>
-              <option>Lat.</option>
-              <option>Long.</option>
-              <option>Ref. R</option>
-              <option>Ref. G</option>
-              <option>Ref. B</option>
+              <option value="">Time</option>
+              <option value="">City</option>
+              <option value="">Temp.</option>
+              <option value="">PH</option>
+              <option value="">Depth</option>
+              <option value="">O2</option>
+              <option value="">Conductivity</option>
+              <option value="">Turbidity</option>
+              <option value="">Chlorophyll</option>
+              <option value="">SPM</option>
+              <option value="">Lat.</option>
+              <option value="">Long.</option>
+              <option value="">Ref. R</option>
+              <option value="">Ref. G</option>
+              <option value="">Ref. B</option>
             </select>
 
-            <select className="h-8 mx-1">
-              <option value="" selected disabled>
+            <select className="h-8 mx-1" defaultValue="">
+              <option value="" disabled>
                 Select Operation
               </option>
               <option value="">&lt;</option>
@@ -122,7 +115,21 @@ const GeoLocation = () => {
               />
             </span>
           </div>
-          <div id="map" className="h-[400px] w-full"></div>
+          <MapContainer center={[23.0225, 72.5714]} zoom={12} className="h-[400px] w-full">
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {markerIcons.map((icon, index) => (
+              <Marker 
+                key={index} 
+                position={features[index].position} 
+                icon={icon} // Use the colored icon
+              >
+                <Tooltip>{features[index].type}</Tooltip>
+              </Marker>
+            ))}
+          </MapContainer>
         </div>
       </main>
     </div>
